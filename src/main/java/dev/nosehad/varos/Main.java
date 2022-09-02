24b45e0d6bb5a1042a6985e2d2fa5c1e0465abb6
nosehad <77919109+nosehad@users.noreply.github.com>
2022-09-02 11:39:13 +0200

    improving file structure

--- src/main/java/dev/nosehad/varos/Main.java
+++ src/main/java/dev/nosehad/varos/Main.java
@@ -1,18 +1,19 @@
 package dev.nosehad.varos;
 
-import dev.nosehad.varos.User.userdata;
 import dev.nosehad.varos.commands.*;
 import dev.nosehad.varos.listener.ChatListener;
-import dev.nosehad.varos.varos.ProtectionListener;
 import dev.nosehad.varos.listener.joinListener;
 import dev.nosehad.varos.utils.YMLConfiguration;
 import dev.nosehad.varos.varos.LVaros;
+import dev.nosehad.varos.varos.ProtectionListener;
 import dev.nosehad.varos.varos.SpawnBoostListener;
 import dev.nosehad.varos.varos.Varos;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutionException;
@@ -60,56 +61,59 @@ public class Main extends JavaPlugin {
     }
 
     static void setupFiles () {
-        File f1 = new File ( "./plugins/Varos" );
-        if(f1.mkdir ()) {
-            if ( !Files.isReadable ( Paths.get ( "./plugins/Varos/config.yml" ) ) ) {
-                try (FileWriter file = new FileWriter ( "./plugins/Varos/config.yml" )) {
-                    file.write ( """
-                                         ## set on true to enable 1.8 combat ##
-                                         old_combat: true
+        if(!new File ( "./plugins/Varos" ).mkdir ())
+            return;
+        if ( Files.isReadable ( Paths.get ( "./plugins/Varos/config.yml" ) ) )
+            return;
+        try (FileWriter file = new FileWriter ( "./plugins/Varos/config.yml" ))
+        {
+            file.write ( """
+                                        ## set on true to enable 1.8 combat ##
+                                        old_combat: true
+                                       
+                                        ## /verify <admin_passwort> to be able to enter admin commands ##
+                                        admin_password: example
+                                         
+                                        ## protection time at game start in minutes ##
+                                        prot_time: 45
+                                         
+                                        ## radius for spawn flight ##
+                                        spawnradius: 50
+                                         
+                                        ## amount of allowed deaths ##
+                                        lives: 3
+                                         
+                                        ## is it allowed to respawn in beds if you have lifes left ##
+                                        bed_respawn: false
+                                         
+                                        ## after this amount of minutes, the worldborder starts shrinking ##
+                                        start_shrinking: 60
                                         
-                                         ## /verify <admin_passwort> to be able to enter admin commands ##
-                                         admin_password: example
-                                          
-                                         ## protection time at game start in minutes ##
-                                         prot_time: 45
-                                          
-                                         ## radius for spawn flight ##
-                                         spawnradius: 50
-                                          
-                                         ## amount of allowed deaths ##
-                                         lives: 3
-                                          
-                                         ## is it allowed to respawn in beds if you have lifes left ##
-                                         bed_respawn: false
-                                          
-                                         ## after this amount of minutes, the worldborder starts shrinking ##
-                                         start_shrinking: 60
+                                        ## worldborder radius at start of the game ##
+                                        wordborder_radius: 5000
+                                        
+                                        ## smallest worldborder radius ##
+                                        min_worldborder_radius: 100
+                                        
+                                        ## player limit per team ##
+                                        max_team_players: 2
                                          
-                                         ## worldborder radius at start of the game ##
-                                         wordborder_radius: 5000
-                                         
-                                         ## smallest worldborder radius ##
-                                         min_worldborder_radius: 100
-                                         
-                                         ## player limit per team ##
-                                         max_team_players: 2
-                                          
-                                         ## set the spawn by using /varos setspawn ##
-                                         
-                                         ~Varos Plugin by _itznosehad_
-                                         """ );
-                    file.flush ( );
-                }
-                catch (IOException e) {
-                    e.printStackTrace ( );
-                }
-            }
+                                        ## set the spawn by using /varos setspawn ##
+                                        
+                                        ~Varos Plugin by _itznosehad_
+                                        """ );
+            file.flush ( );
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace ( );
         }
     }
-    public void startVaros() {
+    public void startVaros()
+    {
         YMLConfiguration configuration = new YMLConfiguration ( "./plugins/Varos/config.yml" );
-        try {
+        try
+        {
             Bukkit.getLogger().info (
                     "Starting Varos Plugin with configuration: \n" +
                             "lives: " + configuration.getString ( "lives" )  + "\n" +
@@ -124,18 +128,18 @@ public class Main extends JavaPlugin {
                             "protection time: " + configuration.getString ( "prot_time" ) + " minutes"  );
             if( Files .isReadable ( Paths.get ( "./plugins/Varos/varos.dat"  ))) {
                 Varos.loadRecent ();
-            } else {
-                new Varos ( Integer.parseInt ( configuration.getString ( "lives" ) ) ,
-                            Integer.parseInt ( configuration.getString ( "spawnradius" ) ) ,
-                            Boolean.parseBoolean ( configuration.getString ( "bed_respawn" ) ) ,
-                            Integer.parseInt ( configuration.getString ( "start_shrinking" ) ) ,
-                            Boolean.parseBoolean ( configuration.getString ( "old_combat" ) ) ,
-                            configuration.getString ( "admin_password" ) ,
-                            Integer.parseInt ( configuration.getString ( "prot_time" ) ) ,
-                            Integer.parseInt ( configuration.getString ( "wordborder_radius" ) ),
-                            Integer.parseInt ( configuration.getString ( "min_worldborder_radius" ) ),
-                            Integer.parseInt ( configuration.getString ( "max_team_players" ) ));
+                return;
             }
+            new Varos ( Integer.parseInt ( configuration.getString ( "lives" ) ) ,
+                        Integer.parseInt ( configuration.getString ( "spawnradius" ) ) ,
+                        Boolean.parseBoolean ( configuration.getString ( "bed_respawn" ) ) ,
+                        Integer.parseInt ( configuration.getString ( "start_shrinking" ) ) ,
+                        Boolean.parseBoolean ( configuration.getString ( "old_combat" ) ) ,
+                        configuration.getString ( "admin_password" ) ,
+                        Integer.parseInt ( configuration.getString ( "prot_time" ) ) ,
+                        Integer.parseInt ( configuration.getString ( "wordborder_radius" ) ),
+                        Integer.parseInt ( configuration.getString ( "min_worldborder_radius" ) ),
+                        Integer.parseInt ( configuration.getString ( "max_team_players" ) ));
         }
         catch (IOException | ExecutionException e) {
             e.printStackTrace ();
